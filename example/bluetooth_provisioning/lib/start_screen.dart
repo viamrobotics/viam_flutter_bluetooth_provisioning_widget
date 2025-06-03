34ca8575d2c8494c877caeb9fb699c9db4e3e976
Kevin Johnson <kevin.johnson@viam.com>
2025-06-03 16:08:41 -0400

    add setup to pass in robot/mainpart at start

--- example/bluetooth_provisioning/lib/start_screen.dart
+++ example/bluetooth_provisioning/lib/start_screen.dart
@@ -1,23 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+// ignore: depend_on_referenced_packages
+import 'package:viam_sdk/viam_sdk.dart';
+// ignore: depend_on_referenced_packages
+import 'package:viam_sdk/protos/app/app.dart';
 
 import 'bluetooth_provisioning_flow.dart';
 import 'view_model.dart';
+import 'consts.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   const StartScreen({super.key});
 
-  void _goToIntroScreenOne(BuildContext context) async {
-    // Navigator.of(context).push(MaterialPageRoute(
-    //   builder: (context) => ChangeNotifierProvider(
-    //     create: (context) => BluetoothProvisioningFlowViewModel(
-    //       viam: AuthService.authenticatedViam,
-    //       robot: Robot(id: ''),
-    //       mainRobotPart: RobotPart(id: ''),
-    //     ),
-    //     builder: (context, child) => BluetoothProvisioningFlow(),
-    //   ),
-    // ));
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  Viam? _viam;
+  Robot? _robot;
+  RobotPart? _mainPart;
+
+  @override
+  void initState() {
+    super.initState();
+    _initViam();
+  }
+
+  void _goToIntroScreenOne(BuildContext context, Viam viam, Robot robot, RobotPart mainPart) async {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ChangeNotifierProvider(
+        create: (context) => BluetoothProvisioningFlowViewModel(
+          viam: viam,
+          robot: robot,
+          mainRobotPart: mainPart,
+        ),
+        builder: (context, child) => BluetoothProvisioningFlow(),
+      ),
+    ));
+  }
+
+  Future<void> _initViam() async {
+    try {
+      _viam = await Viam.withApiKey(Consts.apiKeyId, Consts.apiKey);
+      _createRobot(_viam!);
+    } catch (e) {
+      debugPrint('Error initializing Viam: $e');
+    }
+  }
+
+  Future<void> _createRobot(Viam viam) async {
+    final location = await viam.appClient.createLocation(Consts.organizationId, 'test-location-${Random().nextInt(1000)}');
+    final String robotName = "tester-${Random().nextInt(1000)}";
+    debugPrint('robotName: $robotName, locationId: ${location.name}');
+    final robotId = await viam.appClient.newMachine(robotName, location.id);
+    _robot = await viam.appClient.getRobot(robotId);
+    _mainPart = (await viam.appClient.listRobotParts(robotId)).firstWhere((element) => element.mainPart);
   }
 
   @override
@@ -28,12 +68,15 @@ class StartScreen extends StatelessWidget {
       ),
       body: Center(
         child: FilledButton(
-          onPressed: () => _goToIntroScreenOne(context),
+          onPressed: () => _goToIntroScreenOne(
+            context,
+            _viam!,
+            _robot!,
+            _mainPart!,
+          ),
           child: const Text('Start Flow'),
         ),
       ),
     );
   }
-
-  // TODO: add the api key stuff.. from hotspot example
 }
