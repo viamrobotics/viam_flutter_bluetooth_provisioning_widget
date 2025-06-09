4e57db8e6c92ebc1dbf46204d5c4483f12a6cf7c
Kevin Johnson <kevin.johnson@viam.com>
2025-06-09 14:47:28 -0400

    Update connected_bluetooth_device_screen.dart

--- lib/src/view/connected_bluetooth_device_screen.dart
+++ lib/src/view/connected_bluetooth_device_screen.dart
@@ -13,7 +13,9 @@ class _ConnectedBluetoothDeviceScreenState extends State<ConnectedBluetoothDevic
   @override
   void initState() {
     super.initState();
-    _readNetworkList();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _readNetworkList();
+    });
   }
 
   // would be in view model, but want to handle error and show dialog w/ the context
@@ -21,9 +23,9 @@ class _ConnectedBluetoothDeviceScreenState extends State<ConnectedBluetoothDevic
     try {
       await Provider.of<ConnectedBluetoothDeviceScreenViewModel>(context, listen: false).readNetworkList();
     } catch (e) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
         showErrorDialog(context, title: 'Error reading network list', error: e.toString());
-      });
+      }
     }
   }
 
@@ -114,93 +116,96 @@ class _ConnectedBluetoothDeviceScreenState extends State<ConnectedBluetoothDevic
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<ConnectedBluetoothDeviceScreenViewModel>(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text('Choose your Wi-Fi', style: Theme.of(context).textTheme.titleLarge),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 24.0),
-          child: Text(
-            'Choose the Wi-Fi network you\'d like to use to connect your device.',
-            style: Theme.of(context).textTheme.bodyLarge,
-            maxLines: 2,
-          ),
-        ),
-        viewModel.isLoadingNetworks && viewModel.wifiNetworks.isEmpty
-            ? Expanded(
-                child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  separatorBuilder: (context, index) => const SizedBox(height: 16),
-                  itemCount: 1,
-                  itemBuilder: (context, _) {
-                    return Card(
-                      elevation: 0,
-                      color: const Color(0xFFF5F7F8),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: const ScanningListTile(),
-                    );
-                  },
-                ),
-              )
-            : Expanded(
-                child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  separatorBuilder: (context, index) => const SizedBox(height: 16),
-                  itemCount: viewModel.wifiNetworks.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(color: Colors.grey.shade300),
-                      ),
-                      child: ListTile(
-                        minVerticalPadding: 20,
-                        leading: Icon(_networkIcon(viewModel.wifiNetworks[index]), color: const Color(0xFF8B949E), size: 20),
-                        trailing: viewModel.wifiNetworks[index].isSecure
-                            ? Icon(Icons.lock_outline, color: const Color(0xFF8B949E), size: 20)
-                            : null,
-                        horizontalTitleGap: 16,
-                        title: Text(viewModel.wifiNetworks[index].ssid, style: Theme.of(context).textTheme.bodyLarge),
-                        onTap: () {
-                          if (viewModel.wifiNetworks[index].isSecure) {
-                            _presentPasskeyDialog(viewModel.wifiNetworks[index]);
-                          } else {
-                            viewModel.handleWifiCredentials(viewModel.wifiNetworks[index].ssid, null);
-                          }
-                        },
-                      ),
-                    );
-                  },
+    return Consumer<ConnectedBluetoothDeviceScreenViewModel>(
+      builder: (context, viewModel, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('Choose your Wi-Fi', style: Theme.of(context).textTheme.titleLarge),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 24.0),
+              child: Text(
+                'Choose the Wi-Fi network you\'d like to use to connect your device.',
+                style: Theme.of(context).textTheme.bodyLarge,
+                maxLines: 2,
+              ),
+            ),
+            viewModel.isLoadingNetworks && viewModel.wifiNetworks.isEmpty
+                ? Expanded(
+                    child: ListView.separated(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      separatorBuilder: (context, index) => const SizedBox(height: 16),
+                      itemCount: 1,
+                      itemBuilder: (context, _) {
+                        return Card(
+                          elevation: 0,
+                          color: const Color(0xFFF5F7F8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: const ScanningListTile(),
+                        );
+                      },
+                    ),
+                  )
+                : Expanded(
+                    child: ListView.separated(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      separatorBuilder: (context, index) => const SizedBox(height: 16),
+                      itemCount: viewModel.wifiNetworks.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          child: ListTile(
+                            minVerticalPadding: 20,
+                            leading: Icon(_networkIcon(viewModel.wifiNetworks[index]), color: const Color(0xFF8B949E), size: 20),
+                            trailing: viewModel.wifiNetworks[index].isSecure
+                                ? Icon(Icons.lock_outline, color: const Color(0xFF8B949E), size: 20)
+                                : null,
+                            horizontalTitleGap: 16,
+                            title: Text(viewModel.wifiNetworks[index].ssid, style: Theme.of(context).textTheme.bodyLarge),
+                            onTap: () {
+                              if (viewModel.wifiNetworks[index].isSecure) {
+                                _presentPasskeyDialog(viewModel.wifiNetworks[index]);
+                              } else {
+                                viewModel.handleWifiCredentials(viewModel.wifiNetworks[index].ssid, null);
+                              }
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+            if (!_showingDialog)
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 8),
+                    OutlinedButton.icon(
+                      onPressed: _readNetworkList,
+                      icon: const Icon(Icons.refresh),
+                      label: const Text('Scan network again'),
+                    ),
+                    TextButton(
+                      onPressed: _notSeeingYourNetwork,
+                      child: const Text('Not seeing your network?'),
+                    ),
+                  ],
                 ),
               ),
-        if (!_showingDialog)
-          SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 8),
-                OutlinedButton.icon(
-                  onPressed: _readNetworkList,
-                  icon: const Icon(Icons.refresh),
-                  label: const Text('Scan network again'),
-                ),
-                TextButton(
-                  onPressed: _notSeeingYourNetwork,
-                  child: const Text('Not seeing your network?'),
-                ),
-              ],
-            ),
-          ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
