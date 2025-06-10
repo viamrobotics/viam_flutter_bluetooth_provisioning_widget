a0bdc4a6d76b7a779c089f60dbc10b37f2f26bef
Kevin Johnson <kevin.johnson@viam.com>
2025-06-10 15:14:56 -0400

    add back delay w/ comment

--- example/bluetooth_provisioning/lib/provision_new_machine_screen.dart
+++ example/bluetooth_provisioning/lib/provision_new_machine_screen.dart
@@ -33,6 +33,7 @@ class _ProvisionNewRobotScreenState extends State<ProvisionNewRobotScreen> {
       final robotId = await viam.appClient.newMachine(robotName, location.id);
       final robot = await viam.appClient.getRobot(robotId);
       final mainPart = (await viam.appClient.listRobotParts(robotId)).firstWhere((element) => element.mainPart);
+      await Future.delayed(const Duration(seconds: 3)); // delay is intentional, so you can see the robot name
       if (mounted) {
         _goToBluetoothProvisioningFlow(context, viam, robot, mainPart);
       }
