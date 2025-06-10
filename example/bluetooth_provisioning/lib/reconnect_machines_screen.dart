1001ef0f6b785e07dc9dfb500cb39c11576750ee
Kevin Johnson <kevin.johnson@viam.com>
2025-06-10 15:16:50 -0400

    Reconnect flow added in example (#6)
    * initial
    
    * online
    
    * add status icon
    
    * remove delay..? how did that get added
    
    * add back delay w/ comment

---
+++ example/bluetooth_provisioning/lib/reconnect_machines_screen.dart
@@ -0,0 +1,159 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:viam_flutter_bluetooth_provisioning_widget/viam_flutter_bluetooth_provisioning_widget.dart';
+
+import 'consts.dart';
+
+enum _RobotStatus { online, offline, awaitingSetup, loading }
+
+class _ListRobot {
+  final Robot robot;
+  final String locationName;
+
+  _ListRobot({required this.robot, required this.locationName});
+}
+
+class ReconnectRobotsScreen extends StatefulWidget {
+  const ReconnectRobotsScreen({super.key});
+
+  @override
+  State<ReconnectRobotsScreen> createState() => _ReconnectRobotsScreenState();
+}
+
+class _ReconnectRobotsScreenState extends State<ReconnectRobotsScreen> {
+  Viam? _viam;
+  bool _isLoading = false;
+  List<_ListRobot> _robots = [];
+  final Map<String, _RobotStatus> _robotStatuses = {};
+  Timer? _statusTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRobots();
+    _statusTimer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      _updateRobotStatuses();
+    });
+  }
+
+  @override
+  void dispose() {
+    _statusTimer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _loadRobots() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      _viam = await Viam.withApiKey(Consts.apiKeyId, Consts.apiKey);
+      final locations = await _viam!.appClient.listLocations(Consts.organizationId);
+      final newList = <_ListRobot>[];
+      for (final location in locations) {
+        final locationRobots = await _viam!.appClient.listRobots(location.id);
+        newList.addAll(locationRobots.map((e) => _ListRobot(robot: e, locationName: location.name)));
+      }
+      setState(() {
+        _robots = newList;
+      });
+    } catch (e) {
+      debugPrint('Error loading robots: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _updateRobotStatuses() async {
+    debugPrint('Updating robot statuses');
+    try {
+      final statusFutures = _robots.map((robot) async {
+        try {
+          final reloadRobot = await _viam!.appClient.getRobot(robot.robot.id);
+          final status = await reloadRobot.getStatus();
+          setState(() {
+            _robotStatuses[reloadRobot.id] = status;
+          });
+        } catch (e) {
+          debugPrint('Error getting status for robot ${robot.robot.id}: $e');
+        }
+      });
+      await Future.wait(statusFutures);
+    } catch (e) {
+      debugPrint('Error updating robot statuses: $e');
+    }
+  }
+
+  void _goToBluetoothProvisioningFlow(BuildContext context, Viam viam, Robot robot) async {
+    final mainPart = (await viam.appClient.listRobotParts(robot.id)).firstWhere((element) => element.mainPart);
+    if (context.mounted) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => ChangeNotifierProvider(
+          create: (context) => BluetoothProvisioningFlowViewModel(
+            viam: viam,
+            robot: robot,
+            mainRobotPart: mainPart,
+          ),
+          builder: (context, child) => BluetoothProvisioningFlow(onSuccess: () {
+            Navigator.of(context).pop();
+          }),
+        ),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reconnect Machines'),
+      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator.adaptive(backgroundColor: Colors.black))
+          : ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _robots.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(_robots[index].robot.name),
+                subtitle: Text('location: ${_robots[index].locationName}'),
+                trailing: _robotStatuses[_robots[index].robot.id]?.statusIcon,
+                onTap: () => _goToBluetoothProvisioningFlow(context, _viam!, _robots[index].robot),
+              ),
+            ),
+    );
+  }
+}
+
+extension _RobotStatusCalculation on Robot {
+  Future<_RobotStatus> getStatus() async {
+    final seconds = lastAccess.seconds.toInt();
+    final actual = DateTime.now().microsecondsSinceEpoch / Duration.microsecondsPerSecond;
+    if ((actual - seconds) < 60) {
+      return _RobotStatus.online;
+    }
+
+    if (!lastAccess.hasNanos() && !lastAccess.hasSeconds()) return _RobotStatus.awaitingSetup;
+    if ((actual - seconds) > 60) return _RobotStatus.offline;
+    return _RobotStatus.loading;
+  }
+}
+
+extension _RobotStatusIcon on _RobotStatus {
+  Icon get statusIcon {
+    switch (this) {
+      case _RobotStatus.online:
+        return const Icon(Icons.check_circle, color: Colors.green);
+      case _RobotStatus.offline:
+        return const Icon(Icons.offline_bolt, color: Colors.grey);
+      case _RobotStatus.awaitingSetup:
+        return const Icon(Icons.snooze, color: Colors.blue);
+      case _RobotStatus.loading:
+        return const Icon(Icons.hourglass_empty, color: Colors.black);
+    }
+  }
+}
--- example/bluetooth_provisioning/lib/start_screen.dart
+++ example/bluetooth_provisioning/lib/start_screen.dart
@@ -1,64 +1,20 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-import 'package:viam_flutter_bluetooth_provisioning_widget/viam_flutter_bluetooth_provisioning_widget.dart';
+import 'reconnect_machines_screen.dart';
+import 'provision_new_machine_screen.dart';
 
-import 'consts.dart';
-
-class StartScreen extends StatefulWidget {
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
-  @override
-  State<StartScreen> createState() => _StartScreenState();
-}
-
-class _StartScreenState extends State<StartScreen> {
-  String? _robotName;
-  bool _isLoading = false;
-
-  Future<void> _createRobot() async {
-    setState(() {
-      _isLoading = true;
-    });
-    try {
-      final viam = await Viam.withApiKey(Consts.apiKeyId, Consts.apiKey);
-      final location = await viam.appClient.createLocation(Consts.organizationId, 'test-location-${Random().nextInt(1000)}');
-      final String robotName = "tester-${Random().nextInt(1000)}";
-      setState(() {
-        _robotName = robotName;
-      });
-      debugPrint('robotName: $robotName, locationId: ${location.name}');
-      final robotId = await viam.appClient.newMachine(robotName, location.id);
-      final robot = await viam.appClient.getRobot(robotId);
-      final mainPart = (await viam.appClient.listRobotParts(robotId)).firstWhere((element) => element.mainPart);
-      await Future.delayed(const Duration(seconds: 3));
-      if (mounted) {
-        _goToBluetoothProvisioningFlow(context, viam, robot, mainPart);
-      }
-    } catch (e) {
-      debugPrint('Error initializing Viam: $e');
-    } finally {
-      setState(() {
-        _isLoading = false;
-        _robotName = null;
-      });
-    }
+  void _goToNewMachineFlow(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const ProvisionNewRobotScreen(),
+    ));
   }
 
-  void _goToBluetoothProvisioningFlow(BuildContext context, Viam viam, Robot robot, RobotPart mainPart) async {
+  void _goToReconnectMachinesFlow(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => ChangeNotifierProvider(
-        create: (context) => BluetoothProvisioningFlowViewModel(
-          viam: viam,
-          robot: robot,
-          mainRobotPart: mainPart,
-        ),
-        builder: (context, child) => BluetoothProvisioningFlow(onSuccess: () {
-          Navigator.of(context).pop();
-        }),
-      ),
+      builder: (context) => const ReconnectRobotsScreen(),
     ));
   }
 
@@ -72,11 +28,14 @@ class _StartScreenState extends State<StartScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (_robotName != null) Text('Provisioning machine named: $_robotName'),
-            if (_robotName != null) const SizedBox(height: 16),
             FilledButton(
-              onPressed: _createRobot,
-              child: _isLoading ? const CircularProgressIndicator.adaptive(backgroundColor: Colors.white) : const Text('Start Flow'),
+              onPressed: () => _goToNewMachineFlow(context),
+              child: const Text('New Machine Flow'),
+            ),
+            const SizedBox(height: 16),
+            FilledButton(
+              onPressed: () => _goToReconnectMachinesFlow(context),
+              child: const Text('Reconnect Machines'),
             ),
           ],
         ),
