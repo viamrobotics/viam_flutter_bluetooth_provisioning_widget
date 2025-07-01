36f616cf6407cf1f6d3eb66cd14c46cc0283b95a
Kevin Johnson <kevin.johnson@viam.com>
2025-07-01 19:00:55 -0400

    Update consts.dart

--- example/lib/consts.dart
+++ example/lib/consts.dart
@@ -4,6 +4,9 @@ class Consts {
   static const String apiKey = '';
 
   static const String organizationId = '';
-  // https://github.com/viamrobotics/agent/blob/main/cmd/provisioning-client/opts.go#L20
+
+  /// defaults to 'viamsetup', but if your viam-agent network configuration: https://docs.viam.com/manage/reference/viam-agent/#network_configuration
+  /// has a value set for hotspot_password that will be used instead.
+  /// this pre-shared key is prepended to bluetooth characteristic writes and decoded on the viam-agent side.
   static const String psk = 'viamsetup';
 }
