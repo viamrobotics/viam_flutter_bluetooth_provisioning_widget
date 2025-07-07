3a2756fd6f7852202c4b52840290720d5083f9c4
Kevin Johnson <kevin.johnson@viam.com>
2025-07-07 16:41:35 -0400

    rename

--- example/lib/provision_new_machine_screen.dart
+++ example/lib/provision_new_machine_screen.dart
@@ -61,7 +61,7 @@ class _ProvisionNewRobotScreenState extends State<ProvisionNewRobotScreen> {
           isNewMachine: true,
           mainRobotPart: mainPart,
           psk: Consts.psk,
-          connectedBluetoothDeviceRepository: ConnectedBluetoothDeviceRepository(),
+          connectBluetoothDeviceRepository: ConnectBluetoothDeviceRepository(),
         ),
         builder: (context, child) => BluetoothProvisioningFlow(onSuccess: () {
           Navigator.of(context).pop();
