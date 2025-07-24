595d8d49105cd7a86a876ebd4bf8fd11e27d5cd3
Kevin Johnson <kevin.johnson@viam.com>
2025-07-24 15:54:19 -0400

    Revert "more logic in vm!"
    This reverts commit d05a44d83df785cda7cf1c7c722c09e6f9af5a2a.

--- lib/src/view_models/bluetooth_provisioning_flow_view_model.dart
+++ lib/src/view_models/bluetooth_provisioning_flow_view_model.dart
@@ -11,11 +11,6 @@ class BluetoothProvisioningFlowViewModel extends ChangeNotifier {
     required this.fragmentId,
     this.agentMinimumVersion = '0.20.0',
     this.copy = const BluetoothProvisioningFlowCopy(),
-    required this.onSuccess,
-    required this.handleAgentConfigured,
-    required this.existingMachineExit,
-    required this.nonexistentMachineExit,
-    required this.agentMinimumVersionExit,
   })  : _mainRobotPart = mainRobotPart,
         _psk = psk;
   final Viam viam;
@@ -32,18 +27,6 @@ class BluetoothProvisioningFlowViewModel extends ChangeNotifier {
   final String _psk;
   BluetoothDevice? get device => connectBluetoothDeviceRepository.device;
 
-  final VoidCallback onSuccess;
-
-  /// agent has indicated the machine is online and has machine credentials
-  /// though it may not be online in app.viam.com yet
-  final VoidCallback handleAgentConfigured;
-
-  final VoidCallback existingMachineExit;
-  final VoidCallback nonexistentMachineExit;
-
-  /// called when the connected machine's agent version is lower (or we can't read it) compared to the agentMinimumVersion in the view model
-  final VoidCallback agentMinimumVersionExit;
-
   Future<void> writeConfig({required String ssid, required String? password}) async {
     await connectBluetoothDeviceRepository.writeConfig(
       viam: viam,
@@ -58,98 +41,5 @@ class BluetoothProvisioningFlowViewModel extends ChangeNotifier {
 
   Future<bool> agentVersionBelowMinimum() async {
     return await connectBluetoothDeviceRepository.isAgentVersionBelowMinimum(agentMinimumVersion);
-  }
-
-  Future<bool> isDeviceConnectionValid(BuildContext context, BluetoothDevice device) async {
-    try {
-      // agent minimum check
-      if (await agentVersionBelowMinimum() && context.mounted) {
-        // disconnect the device to avoid any `pairing request` dialogs.
-        device.disconnect();
-        _agentMinimumVersionDialog(context);
-        return false;
-      }
-      // status check
-      final status = await device.readStatus();
-      if (isNewMachine && status.isConfigured && context.mounted) {
-        _avoidOverwritingExistingMachineDialog(context);
-        return false;
-      } else if (!isNewMachine && !status.isConfigured && context.mounted) {
-        _reconnectingNonexistingMachineDialog(context);
-        return false;
-      }
-      return true;
-    } catch (e) {
-      debugPrint('Error reading device status: $e');
-      return false; // could be valid, but undetermined without reading status
-    }
-  }
-
-  Future<void> _avoidOverwritingExistingMachineDialog(BuildContext context) async {
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(copy.existingMachineDialogTitle),
-          content: Text(
-            copy.existingMachineDialogSubtitle,
-          ),
-          actions: <Widget>[
-            OutlinedButton(
-              child: Text(copy.existingMachineDialogCta),
-              onPressed: () {
-                Navigator.pop(context);
-                existingMachineExit();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _agentMinimumVersionDialog(BuildContext context) async {
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(copy.agentIncompatibleDialogTitle),
-          content: Text(copy.agentIncompatibleDialogSubtitle),
-          actions: <Widget>[
-            OutlinedButton(
-              child: Text(copy.agentIncompatibleDialogCta),
-              onPressed: () {
-                Navigator.pop(context);
-                agentMinimumVersionExit();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _reconnectingNonexistingMachineDialog(BuildContext context) async {
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(copy.machineNotFoundDialogTitle),
-          content: Text(copy.machineNotFoundDialogSubtitle),
-          actions: <Widget>[
-            OutlinedButton(
-              child: Text(copy.machineNotFoundDialogCta),
-              onPressed: () {
-                Navigator.pop(context);
-                nonexistentMachineExit();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 }
