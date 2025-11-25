85e24efab83e0569bf6762cfaf66058d8fb76d3e
Kevin Johnson <kevin.johnson@viam.com>
2025-11-25 15:31:15 -0500

    Update generate_mocks.dart

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -1,6 +1,5 @@
 import 'package:viam_flutter_bluetooth_provisioning_widget/viam_flutter_bluetooth_provisioning_widget.dart';
 import 'package:mockito/annotations.dart';
-import 'package:flutter/material.dart';
 import 'package:viam_sdk/src/app/app.dart';
 import 'package:viam_sdk/src/gen/google/protobuf/timestamp.pb.dart';
 
